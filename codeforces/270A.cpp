457402989ec35398e97e5ae66b2e29f625030e15
anonymouslucky <vickybehera332@gmail.com>
2018-06-21 16:36:53 +0530

    Create 270A.cpp

---
+++ codeforces/270A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 400010
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+	
+	lli t,a;
+	
+	cin>>t;
+	while(t--)
+	{
+		cin>>a;
+		if(360%(180-a)==0)
+			cout<<"YES"<<endl;
+		else
+			cout<<"NO"<<endl;
+	}
+	
+	return 0;
+}
