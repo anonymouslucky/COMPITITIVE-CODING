0a9df5533f4014e9b075820444b0180dfbf104b7
anonymouslucky <vickybehera332@gmail.com>
2018-06-21 16:44:14 +0530

    Create 822A.cpp

---
+++ codeforces/822A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 400010
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+	
+	lli a,b,i,ans;
+	cin>>a>>b;
+	if(a>b)
+		swap(a,b);
+	
+	ans=1;	
+	rep(i,1,a)
+		ans*=i;
+	cout<<ans<<endl;
+	return 0;
+}
