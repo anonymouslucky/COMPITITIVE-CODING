a8a0f5155ab1411dee921a09db3300e1bbdbcdf9
Lucky Behera <vickybehera332@gmail.com>
2018-06-23 15:29:49 +0530

    Create 620A.cpp

---
+++ codeforces/620A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 10000010
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	lli x1,x2,y1,y2;
+	cin>>x1>>y1;
+	cin>>x2>>y2;
+	
+	cout<<max(abs(x2-x1),abs(y2-y1))<<endl;
+	return 0;
+}
