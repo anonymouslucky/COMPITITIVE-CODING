458cd6f52f236373dcd0ae2b3334d160de2f2ade
Lucky Behera <vickybehera332@gmail.com>
2018-06-23 12:11:28 +0530

    Create 664A.cpp

---
+++ codeforces/664A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 10000010
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string str1,str2;
+	cin>>str1>>str2;
+	
+	if(str1 == str2)
+		cout<<str1<<endl;
+	else
+		cout<<"1"<<endl;
+}
