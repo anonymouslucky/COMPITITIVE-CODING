a1fd3d661c5cf90045bb191e192132f90a95a050
Lucky Behera <vickybehera332@gmail.com>
2018-06-23 17:27:16 +0530

    Create 584A.cpp

---
+++ codeforces/584A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 10000010
+#define mod 1000000007
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	lli n,t,i;
+	
+	cin>>n>>t;
+	
+	if(n==1 && t==10)
+	{
+		cout<<"-1"<<endl;
+		return 0;
+	}
+	
+	if(t == 10)
+	{
+		rep(i,1,n-1)
+			cout<<"1";
+		cout<<"0";
+		return 0;
+	}
+	
+	rep(i,1,n)
+		cout<<t;
+	
+	return 0;
+}
