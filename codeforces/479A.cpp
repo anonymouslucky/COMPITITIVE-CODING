3d7700fb6b3a3f56dee6015ea1f178cf63485e7b
Lucky Behera <vickybehera332@gmail.com>
2018-06-23 16:43:27 +0530

    Create 479A.cpp

---
+++ codeforces/479A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define lli long long int
+#define ULL unsigned long long int
+#define pb push_back
+#define INF 1e18
+#define N 10000010
+#define mod 1000000007
+#define tol 1e-12
+#define P pair
+#define mk make_pair
+#define rep(i,lower,upper) for(i=lower;i<=upper;i++)
+#define repr(i,upper,lower) for(i=upper;i>=lower;i--)
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	lli a,b,c,ans=0;
+	
+	cin>>a>>b>>c;
+	
+	ans = max(ans,a+b+c);
+	ans = max(ans,a*b*c);
+	ans = max(ans,a+b*c);
+	ans = max(ans,a*b+c);
+	ans = max(ans,(a+b)*c);
+	ans = max(ans,a*(b+c));
+	
+	cout<<ans<<endl;
+	
+	return 0;
+}
